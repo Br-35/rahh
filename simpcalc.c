5578fbcf3c56ae5954a90c3abc335a2768c3f86d
Brian <garbramm@gmail.com>
2025-02-08 16:07:41 +0800

    funcier

--- simpcalc.c
+++ simpcalc.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
 
+void inputNum(float *input) {
+	printf("Input a number: ");
+    scanf("%f", input);
+}
+
+void getOp(char *op,int *nop) {
+	switch (*op) {
+        case '+': *nop = 0; break;
+        case '-': *nop = 1; break;
+        case '*': *nop = 2; break;
+        case '/': *nop = 3; break;
+    }
+}
+
+void doOp(int nop, float input1, float input2, float *output) {
+	switch (nop) {
+        case 0: *output = input1 + input2; break;
+        case 1: *output = input1 - input2; break;
+        case 2: *output = input1 * input2; break;
+        case 3: *output = input1 / input2; break;
+    }
+}    
+
 int main () {
     float input1, input2, output;
     char op;
+    int nop;
 
-    printf("Input a number: ");
-    scanf("%f", &input1);
+//    printf("Input a number: ");
+//    scanf("%f", &input1);
+	inputNum(&input1);
 
     printf("Choose op (+ - * or /): ");
     scanf("%c", &op);
     scanf("%c", &op);
 
-    switch (op) {
-        case '+': op = 0; break;
-        case '-': op = 1; break;
-        case '*': op = 2; break;
-        case '/': op = 3; break;
-    }
+    getOp(&op, &nop);
 
-    printf("Input 2nd number: ");
-    scanf("%f", &input2);
-
-    switch (op) {
-        case 0: output = input1 + input2; break;
-        case 1: output = input1 - input2; break;
-        case 2: output = input1 * input2; break;
-        case 3: output = input1 / input2; break;
-    }
+//    printf("Input a number: ");
+//    scanf("%f", &input2);
+	inputNum(&input2);
+	
+    doOp(nop, input1, input2, &output);
 
     printf("%.2f", output);
 }
